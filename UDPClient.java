2db0ee70ce25df900dfe59cae487e3d5b4780b2f
Aryan <ashirazi@hrwize.com>
2024-03-20 19:07:52 -0400

    client nows runs in a loop to allow sending multiple commands, will break out of loop when receiving EXIT command or timeout. Also added publish and remove commands.

--- UDPClient.java
+++ UDPClient.java
@@ -1,34 +1,104 @@
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
 
 public class UDPClient {
     public static void main(String[] args) {
+        DatagramSocket socket = null;
         try {
-            DatagramSocket socket = new DatagramSocket();
+            socket = new DatagramSocket();
             socket.setSoTimeout(5000);
 
             InetAddress serverAddress = InetAddress.getByName("localhost");
             int serverPort = 2308;
-            
-            String message1 = String.format("REGISTER | 6 | Client6 | %s | %d", serverAddress, serverPort);
-            System.out.println("message: " + message1);
-            
-            byte[] sendData = message1.getBytes();
-            
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
-            socket.send(sendPacket);
 
-            byte[] receiveData = new byte[1024];
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            socket.receive(receivePacket);
+            Scanner scanner = new Scanner(System.in);
+            File directory = new File(".");
+            List<String> filenames = new ArrayList<>();
+            
+            loop: while(!socket.isClosed()) {
+                // Displaying menu options
+                System.out.println("Select an option:");
+                System.out.println("1. REGISTER");
+                System.out.println("2. PUBLISH");
+                System.out.println("3. DEREGISTER");
+                System.out.println("4. REMOVE");
+                System.out.println("5. EXIT");
+                System.out.print("Enter your choice: ");
 
-            String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
-            System.out.println("Server: " + receivedMessage);
+                // Reading user input
+                int choice = scanner.nextInt();
+                
+                String message = "";
 
-            socket.close();
+                switch (choice) {
+                    case 1:
+                        message = String.format("REGISTER | 6 | Client6 | NAME | | %s | %d", serverAddress, serverPort);
+                        System.out.println("message: " + message);
+                        break;
+
+                    case 2:
+                        File[] files = directory.listFiles();
+                        if (files != null) {
+                            for (File file : files) {
+                                if (file.isFile()) {
+                                    filenames.add(file.getName());
+                                }
+                            }
+                        }
+                        
+                        StringBuilder concatenatedFilenames = new StringBuilder();
+                        for (int i = 0; i < filenames.size(); i++) {
+                            concatenatedFilenames.append(filenames.get(i));
+                            if (i < filenames.size() - 1) {
+                                concatenatedFilenames.append(" | ");
+                            }
+                        }
+
+                        message = String.format("PUBLISH | 6 | Client6 | FileName1 | %s",concatenatedFilenames.toString());
+                        System.out.println("message: " + message);
+                        break;
+                    case 3:
+                        message = String.format("DEREGISTER | 6 | Client6");
+                        System.out.println("message: " + message);
+                        break;
+                    
+                    case 4:
+                        String fileToRemove = "Main.java | UDPClient.java | UDPServer.java"; 
+                        message = String.format("REMOVE | 6 | Client6 | %s", fileToRemove);
+                        System.out.println("message: " + message);
+                        break;
+
+                    case 5:
+                        break loop;
+
+                    default:
+                        System.out.println("Invalid choice!");
+                }
+            
+                byte[] sendData = message.getBytes();
+                
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+                socket.send(sendPacket);
+
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+
+                String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Server: " + receivedMessage);
+            }
         } catch (SocketTimeoutException e) {
             System.err.println("Timeout occured: Server did not respond.");
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        finally {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
         }
     }
 }
--- UDPServer.java
+++ UDPServer.java
@@ -6,28 +6,40 @@ public class UDPServer {
     static private List<Client> clients = new ArrayList<>();
 
     public static void main(String[] args) {
-        //List<TCPClient> clients = new ArrayList<>();
         DatagramSocket socket = null;
         try {
             socket = new DatagramSocket(2308);
             while(!socket.isClosed()) {
                 byte[] buffer = new byte[1024];
-
-                // Receive the first message
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 socket.receive(request);
 
                 String message = new String(request.getData(), 0, request.getLength());
-                String[] parts = message.split("\\|");
-
-                // parse register request: REGISTER | RQ# | ClientName | IP_Addr | socket#
-                String commandType = parts[0].trim();
-                Integer REQnumb = Integer.parseInt(parts[1].trim());
-                String clientName = parts[2].trim();
                 InetAddress clientAddress = request.getAddress();
                 Integer clientPort = request.getPort();
-                
-                String response = handleCommand(commandType, REQnumb, clientName, clientAddress, clientPort);
+
+                String[] parts = message.split("\\|");
+                String commandType = parts[0].trim();
+                String response = "";
+
+                switch (commandType) {
+                    case "REGISTER":
+                        response = handleRegisterCommand(parts, clientAddress, clientPort);
+                        break;
+                    case "DEREGISTER":
+                        response = handleDeregisterCommand(parts);
+                        break;
+                    case "PUBLISH":
+                        response = handlePublishCommand(parts);
+                        break;
+                    case "REMOVE":
+                        response = handleRemoveCommand(parts);
+                        break;
+                    default:
+                        System.out.println("Unknown command type");
+                        // Handle unknown command type
+                        break;
+                }
 
                 byte[] sendData = response.getBytes();
                 DatagramPacket reply = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
@@ -43,61 +55,130 @@ public class UDPServer {
         }   
     }
 
-    public static String handleCommand(String commandType, Integer REQnumb, String clientName, InetAddress clientAddress, int clientPort) {
+    private static String handleRemoveCommand(String[] parts) {
         String response = "";
-        switch (commandType) {
-            case "REGISTER":
-                boolean clientNameExists = false;
-                if(clients.isEmpty()) {
-                    Client newClient = new Client(clientName, clientAddress, clientPort);
-                    clients.add(newClient);
-                    response = String.format("REGISTERED | %s", REQnumb);
-                } else {
-                    for (Client client : clients) {
-                        String name = client.getClientName();
-                        if (name != null && name.equals(clientName)) { //register denied
-                            clientNameExists = true;
-                            break;
-                        }
+        Integer REQnumb = Integer.parseInt(parts[1].trim());
+        String clientName = parts[2].trim();
+        boolean clientNameDNE = true;
+        if(!clients.isEmpty()){
+            for (Client client : clients) {
+                String name = client.getClientName();
+                if (name != null && name.equals(clientName)) { 
+                    for (int i = 3; i < parts.length; i++) {
+                        client.removeFileFromList(parts[i]);   
                     }
-                    if (!clientNameExists) {
-                        Client newClient = new Client(clientName, clientAddress, clientPort);
-                        clients.add(newClient);
-                        response = String.format("REGISTERED | %s", REQnumb);
-                    } else {
-                        response = String.format("REGISTER-DENIED | %s | Name already taken by another Client",  REQnumb);
-                    }
+                    response = String.format("REMOVED | %s", REQnumb);
+                    clientNameDNE = false;
+                    break;
                 }
-                break;
-            case "DEREGISTER":
-                boolean clientNameDNE = true;
-                Client clientToRemove = null;
-                if(!clients.isEmpty()) {
-                    for (Client client : clients) {
-                        String name = client.getClientName();
-                        if (name != null && name.equals(clientName)) { 
-                            clientToRemove = client;
-                            clientNameDNE = false;
-                            break;
-                        }
-                    }
-                    if (!clientNameDNE) {
-                        response = String.format(" De-registering client: %s ", clientToRemove.getClientName());
-                        clients.remove(clientToRemove);
-                    } else {
-                        response = String.format("%s is not registered", clientToRemove.getClientName());
-                    }
-                }
-                break;
-            // Add additional cases as needed
-            default:
-                System.out.println("Unknown command type");
-                // Handle unknown command type
-                break;
+            }
+            if (clientNameDNE) {
+                response = String.format("REMOVE-DENIED | %s | Client Name does not exist",  REQnumb);
+            }
+        } else {
+            System.out.println("Client list is empty.");
         }
+        System.out.println("Files available for share:");
+        for (Client client : clients) {
+            client.printFileNameFromList();
+        }
+        System.out.println("----------------------------------------------------------------------");
+        return response;
+    }
+
+    private static String handlePublishCommand(String[] parts) {
+        String response = "";
+        Integer REQnumb = Integer.parseInt(parts[1].trim());
+        String clientName = parts[2].trim();
+        boolean clientNameDNE = true;
+        if(!clients.isEmpty()){
+            for (Client client : clients) {
+                String name = client.getClientName();
+                if (name != null && name.equals(clientName)) { 
+                    for (int i = 3; i < parts.length; i++) {
+                        client.addFileToList(parts[i]);   
+                    }
+                    response = String.format("PUBLISHED | %s", REQnumb);
+                    clientNameDNE = false;
+                    break;
+                }
+            }
+            if (clientNameDNE) {
+                response = String.format("PUBLISH-DENIED | %s | Client Name does not exist",  REQnumb);
+            }
+        } else {
+            System.out.println("Client list is empty.");
+        }
+        System.out.println("Files available for share:");
+        for (Client client : clients) {
+            client.printFileNameFromList();
+        }
+        System.out.println("----------------------------------------------------------------------");
+        return response;
+    }
+
+    private static String handleDeregisterCommand(String[] parts) {
+        Integer REQnumb = Integer.parseInt(parts[1].trim());
+        String clientName = parts[2].trim();
+        String response = "";
+        boolean clientNameDNE = true;
+        Client clientToRemove = null;
+        if(!clients.isEmpty()) {
+            for (Client client : clients) {
+                String name = client.getClientName();
+                if (name != null && name.equals(clientName)) { 
+                    clientToRemove = client;
+                    clientNameDNE = false;
+                    break;
+                }
+            }
+            if (!clientNameDNE) {
+                response = String.format("De-registering client: %s | %d", clientToRemove.getClientName(), REQnumb);
+                clients.remove(clientToRemove);
+            } else {
+                response = String.format("%s is not registered | %d", clientToRemove.getClientName(), REQnumb);
+            }
+        } else {
+            System.out.println("Client list is empty.");
+        }
+        System.out.println("List of registered clients:");
+        for (Client client : clients) {
+            System.out.println(client.getClientName());
+        }
+        System.out.println("----------------------------------------------------------------------");
+        return response;
+    }
+
+    private static String handleRegisterCommand(String[] parts,InetAddress clientAddress, Integer clientPort) {
+        Integer REQnumb = Integer.parseInt(parts[1].trim());
+        String clientName = parts[2].trim();
+        String response = "";
+        boolean clientNameExists = false;
+        if(clients.isEmpty()) {
+            Client newClient = new Client(clientName, clientAddress, clientPort);
+            clients.add(newClient);
+            response = String.format("REGISTERED | %s", REQnumb);
+        } else {
+            for (Client client : clients) {
+                String name = client.getClientName();
+                if (name != null && name.equals(clientName)) { //register denied
+                    clientNameExists = true;
+                    break;
+                }
+            }
+            if (!clientNameExists) {
+                Client newClient = new Client(clientName, clientAddress, clientPort);
+                clients.add(newClient);
+                response = String.format("REGISTERED | %s", REQnumb);
+            } else {
+                response = String.format("REGISTER-DENIED | %s | Name already taken by another Client",  REQnumb);
+            }
+        }
+        System.out.println("List of registered clients:");
+        for (Client client : clients) {
+            System.out.println(client.getClientName());
+        }
+        System.out.println("----------------------------------------------------------------------");
         return response;
     }
 }
-
-
-
